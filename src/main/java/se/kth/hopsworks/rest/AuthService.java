a808af0bd5798fec6c0dc63c5eef00d91f33d3bb
Jim Dowling <jdowling@sics.se>
2016-03-20 20:01:49 +0100

    check email is null when logging in

--- src/main/java/se/kth/hopsworks/rest/AuthService.java
+++ src/main/java/se/kth/hopsworks/rest/AuthService.java
@@ -87,7 +87,16 @@ public class AuthService {
     req.getServletContext().log("SecurityContext in user role: " + sc.isUserInRole("BBC_USER"));
     req.getServletContext().log("SecurityContext in sysadmin role: " + sc.isUserInRole("SYS_ADMIN"));
     JsonResponse json = new JsonResponse();
+
+	if (email == null || email.isEmpty()) {
+	  throw new AppException(Response.Status.UNAUTHORIZED.getStatusCode(),
+                    "Email address field cannot be empty");
+	}
     Users user = userBean.findByEmail(email);
+	if (user == null) {
+	  throw new AppException(Response.Status.UNAUTHORIZED.getStatusCode(),
+                    "Unrecognized email address. Have you registered yet?");
+	}
     String newPassword = null;
     // Add padding if custom realm is disabled
     if (otp == null || otp.isEmpty() && user.getMode() == PeopleAccountStatus.MOBILE_USER.getValue()) {
@@ -98,11 +107,13 @@ public class AuthService {
       newPassword = password + otp;
     } else if (otp.length() == AuthenticationConstants.YUBIKEY_OTP_PADDING.length() && user.getMode() == PeopleAccountStatus.YUBIKEY_USER.getValue()) {
         newPassword = password + otp + AuthenticationConstants.YUBIKEY_USER_MARKER;
-    }
+    } else {
+	  newPassword = password;
+	}
     
     //only login if not already logged in...
     if (sc.getUserPrincipal() == null) {
-      if (user != null && statusValidator.checkStatus(user.getStatus())) {
+      if (statusValidator.checkStatus(user.getStatus())) {
         try {
 
           req.getServletContext().log("going to login. User status: " + user.
